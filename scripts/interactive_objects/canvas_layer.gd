4c5f6a4b279d2a431e7a19a82e3332e6aa83a5b2
jbuehler23 <jbuehler23@gmail.com>
2025-03-23 23:39:12 +0000

    feat: add scripts for listening to oven_game interactions!

--- scripts/interactive_objects/canvas_layer.gd
+++ scripts/interactive_objects/canvas_layer.gd
@@ -3,9 +3,8 @@ extends CanvasLayer
 func _ready() -> void:
 	visible = false
 
-func _on_cutting_board_start_game() -> void:
+
+
+
+func _on_oven_start_game() -> void:
 	visible = true
-
-
-func _on_quick_slice_minigame_finished(result: Variant) -> void:
-	visible = false
--- scripts/interactive_objects/oven_game.gd
+++ scripts/interactive_objects/oven_game.gd
@@ -47,3 +47,6 @@ func _input(event: InputEvent) -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_oven_start_game() -> void:
+	pass # Replace with function body.
